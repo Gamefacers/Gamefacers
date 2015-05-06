10f4f7d131ca15e6cdeb57c0c6ff662982302c2a
voffamamma <voffamamma@gmail.com>
2015-05-06 22:41:42 +0000

    New Name
    fo reals

---
+++ GameFacers/GameFacers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameFacers.Startup))]
+namespace GameFacers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
