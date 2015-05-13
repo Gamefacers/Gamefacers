07c59fdc81318a290a1f5cf45161ffab9a556b36
Dalli170 <dalmann90@gmail.com>
2015-05-13 14:15:33 +0000

    This is some heavy shit

---
+++ Gamefacers-Solution/Gamefacers/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Gamefacers.Repositories;
+
+namespace Gamefacers.Controllers
+{
+    public class ProfileController : Controller
+    {
+        IFriendshipRepo friendRepo = new FriendshipRepo();
+        IGroupRepo groupRepo = new GroupRepo();
+       
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
